be42c6f5e75b1b5154f2d6056acebab50884c4ae
Zloteanu Nikita <nikita@kuwaitnet.com>
2016-02-29 17:22:52 +0000

    Initial Commit

---
+++ LetoMovie/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LetoMovie
+//
+//  Created by Zloteanu Nikita on 2/29/16.
+//  Copyright © 2016 nichita. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LetoMovie/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LetoMovie
+//
+//  Created by Zloteanu Nikita on 2/29/16.
+//  Copyright © 2016 nichita. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

ec86c527b0991d8a2ce927a66bd774325b0e6318
Zloteanu Nikita <nikita@kuwaitnet.com>
2016-03-01 23:22:32 +0000

    Beggining work on v2

---
+++ LetoMovie/CineworldAPI.h
@@ -0,0 +1,13 @@
+//
+//  CineworldAPI.h
+//  LetoMovie
+//
+//  Created by Zloteanu Nikita on 3/1/16.
+//  Copyright © 2016 nichita. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CineworldAPI : NSObject
+
+@end
---
+++ LetoMovie/CineworldAPI.m
@@ -0,0 +1,13 @@
+//
+//  CineworldAPI.m
+//  LetoMovie
+//
+//  Created by Zloteanu Nikita on 3/1/16.
+//  Copyright © 2016 nichita. All rights reserved.
+//
+
+#import "CineworldAPI.h"
+
+@implementation CineworldAPI
+
+@end
